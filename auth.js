11e605411c51e775dce359cd740ace871eb92d44
zalfazahirah <74364695+zalfazahirah@users.noreply.github.com>
2020-11-13 13:34:19 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+   consumer_key: process.env.C_KEY,
+   consumer_secret: process.env.C_SECRET,
+   access_token: process.env.A_TOKEN,
+   access_token_secret: process.env.A_SECRET,
+   timeout_ms: 60*1000
+}
